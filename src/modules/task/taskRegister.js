bc7beff1261489e410c9bbeb19369c1c77e8e08c
adpol95 <adpol95@gmail.com>
2023-03-15 20:11:22 +0300

    Server upgrade 1

--- src/modules/task/taskRegister.js
+++ src/modules/task/taskRegister.js
@@ -4,12 +4,19 @@ function taskRegister(req, res) {
   const user = new User({
     value: req.body.name,
     line: req.body.line,
-  })
+  });
 
   user
     .save()
-    .then(() => res.status(200).json('Task added'))
-    .catch((err) => res.status(400).json(err))
+    .then(() => {
+      res.status(200).json('Task added')
+    })
+    .catch((err) => {
+      res.status(400).json(err)
+    })
+    .finally(() => {
+      console.log('GOT IT');
+    });
 }
 
 module.exports = taskRegister;
