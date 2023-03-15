934ad16db42bb2a66ac030640783c215801a5841
adpol95 <adpol95@gmail.com>
2023-03-15 14:34:57 +0300

    Few changes 6

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const bodyPrs = require('./modules/core/bodyParser');
+const bodyPrs = require('./core/bodyParser');
 //const loggerM = require('./modules/core/logger');
 //const errorHandler = require('./modules/core/errorHandler');
 //const cors = require('./modules/core/cors');
-const { router } = require('./modules/home/front/routes');
-const db = require('./modules/core/db');
+const { router } = require('./home/front/routes');
+const db = require('./core/db');
 
 const PORT = 5000;
 
