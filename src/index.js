dcb739eaa3c6911b2ee0e5966139c50212eda79a
adpol95 <adpol95@gmail.com>
2023-03-17 15:07:20 +0300

    Disable CORS for testing

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const app = express();
 const bodyPrs = require('./modules/core/bodyParser');
 const loggerM = require('./modules/core/logger');
 const errorHandler = require('./modules/core/errorHandler');
-const cors = require('./modules/core/cors');
+// const cors = require('./modules/core/cors');
 const { router } = require('./modules/core/routes');
 const db = require('./modules/core/db');
 const { dirAdaptive } = require('./modules/core/dirAdaptive')
 
 const PORT = 5000;
 
-cors(app);
+// cors(app);
 db();
 bodyPrs(app);
 router(app);
